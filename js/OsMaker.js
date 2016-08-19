2157f4896ee286cdebb40954446d7ba713e8d63a
Joseph Hovik <jbhovik@gmail.com>
2016-08-18 20:08:49 -0600

    Added UI buttons

--- js/OsMaker.js
+++ js/OsMaker.js
@@ -1,12 +1,17 @@
-var myLinux = new Linux('Linux is open source!', 'Tux the penguin');
-var myMac = new Mac('Think different', 'Behold the Apple Watch!');
-var myWindows = new Windows('We don\'t have a slogan, but we have Xbox!' , 'Others may have a lot, but we\'ve got Steve Ballmer!');
+createLinuxOs = function(){
+  var myLinux = new Linux('Linux is open source!', 'Tux the penguin');
+  myLinux.printMessage();
+  myLinux.printMascot();
+}
 
-myLinux.printMessage();
-myLinux.printMascot();
+createMacOs = function(){
+  var myMac = new Mac('Think different', 'Behold the Apple Watch!');
+  myMac.printMessage();
+  myMac.showAppleWatch();
+}
 
-myMac.printMessage();
-myMac.showAppleWatch();
-
-myWindows.printMessage();
-myWindows.printTheBallmer();
+createWindowsOs = function(){
+  var myWindows = new Windows('We don\'t have a slogan, but we have Xbox!' , 'Others may have a lot, but we\'ve got Steve Ballmer!');
+  myWindows.printMessage();
+  myWindows.printTheBallmer();
+}
