47a892014ec4058e9fdb700da5047bf716faa12d
Joseph Hovik <jbhovik@gmail.com>
2016-08-23 22:45:10 -0600

    Added polymorphism to OsPage.html

--- js/Os.js
+++ js/Os.js
@@ -2,6 +2,10 @@ function Os(message) {
   this.message = message;
 }
 
+Os.create = function(){
+  return new Os('Welcome to the generic OS! I have no attribute.');
+}
+
 Os.prototype.printMessage = function(){
   alert(this.message);
 };
--- js/OsMaker.js
+++ js/OsMaker.js
@@ -1,56 +1,19 @@
-var myLinux;
-var myMac;
-var myWindows;
-var myOs;
-
-createOs = function(){
-  if (myOs instanceof Os === false) {
-    myOs = new Os('Welcome to the generic OS! I have no attribute.');
+createOs = function(typeOfOs){
+  if (typeOfOs === 'Os') {
+    var myOs = Os.create();
+    return myOs;
+  }
+  else if (typeOfOs === 'Linux'){
+    var myLinux = Linux.create();
+    return myLinux;
+  }
+  else if (typeOfOs === 'Mac'){
+    var myMac = Mac.create();
+    return myMac;
+  }
+  else if (typeOfOs === 'Windows'){
+    var myWindows = Windows.create();
+    return myWindows;
   }
 }
 
-printOsMessage = function(){
-  myOs.printMessage();
-}
-
-createLinuxOs = function(){
-  if (myLinux instanceof Os === false) {
-    myLinux = Linux.create();
-  }
-}
-
-printLinuxMessage = function(){
-  myLinux.printMessage();
-}
-
-printLinuxMascot = function(){
-  myLinux.printMascot();
-}
-
-createMacOs = function(){
-  if (myMac instanceof Os === false) {
-    myMac = Mac.create();
-  }
-}
-
-printMacMessage = function(){
-  myMac.printMessage();
-}
-
-printMacAttribute = function(){
-  myMac.showAppleWatch();
-}
-
-createWindowsOs = function(){
-  if (myWindows instanceof Os === false) {
-    myWindows = Windows.create();
-  }
-}
-
-printWindowsMessage = function(){
-  myWindows.printMessage();
-}
-
-printWindowsStarPlayer = function(){
-  myWindows.printTheBallmer();
-}
